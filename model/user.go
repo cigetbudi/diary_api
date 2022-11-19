c32acd04197244ffe940826c6f867649c1091f61
Sigit <cigetbudi@gmail.com>
2022-11-20 02:22:56 +0700

    Add AuthInput models and Registration

--- model/user.go
+++ model/user.go
@@ -1,10 +1,36 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"diary_api/database"
+	"html"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"size:255;not null;unique" json:"username"`
 	Password string `gorm:"size:255;not null;" json:"-"`
 	Entries  []Entry
+}
+
+func (u *User) Save() (*User, error) {
+	err := database.Database.Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+func (u *User) BeforeSave(*gorm.DB) error {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(passwordHash)
+	u.Username = html.EscapeString(strings.TrimSpace(u.Username))
+	return nil
 }
