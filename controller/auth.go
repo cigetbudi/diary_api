8e360a1167f6f0a7bd4bc3e17fd8eb772ca906ba
Sigit <cigetbudi@gmail.com>
2022-11-22 13:43:21 +0700

    Add Login Controler

--- controller/auth.go
+++ controller/auth.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"diary_api/helper"
 	"diary_api/model"
 	"net/http"
 
@@ -28,4 +29,32 @@ func Register(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, gin.H{"user": savedUser})
+}
+
+func Login(ctx *gin.Context) {
+	var vm model.AuthInput
+
+	if err := ctx.ShouldBindJSON(&vm); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := model.FindUserByUsername(vm.Username)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = user.ValidatePassword(vm.Password)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	jwt, err := helper.GenerateJWT(user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"jwt": jwt})
 }
