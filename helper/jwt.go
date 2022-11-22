630f43abfe4ca0e8dee57ca0369be54626050ef7
Sigit <cigetbudi@gmail.com>
2022-11-22 18:41:34 +0700

    Add Method and Add Serve Server

--- helper/jwt.go
+++ helper/jwt.go
@@ -2,10 +2,14 @@ package helper
 
 import (
 	"diary_api/model"
+	"errors"
+	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
 
@@ -13,10 +17,58 @@ var privateKey = []byte(os.Getenv("JWT_PRIVATE_KEY"))
 
 func GenerateJWT(user model.User) (string, error) {
 	tokenTTL, _ := strconv.Atoi(os.Getenv("TOKEN_TTL"))
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"id":  user.ID,
 		"iat": time.Now().Unix(),
 		"eat": time.Now().Add(time.Second * time.Duration(tokenTTL)).Unix(),
 	})
 	return token.SignedString(privateKey)
+}
+
+func ValidateJWT(ctx *gin.Context) error {
+	token, err := getToken(ctx)
+	if err != nil {
+		return err
+	}
+	_, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		return nil
+	}
+	return errors.New("token tidak valid")
+}
+
+func CurrentUser(ctx *gin.Context) (model.User, error) {
+	err := ValidateJWT(ctx)
+	if err != nil {
+		return model.User{}, err
+	}
+	token, _ := getToken(ctx)
+	claims, _ := token.Claims.(jwt.MapClaims)
+	userId := uint(claims["id"].(float64))
+
+	user, err := model.FindUserById(userId)
+	if err != nil {
+		return model.User{}, err
+	}
+	return user, nil
+}
+
+func getTokenFromRequest(ctx *gin.Context) string {
+	bearerToken := ctx.Request.Header.Get("Authorization")
+	splitToken := strings.Split(bearerToken, " ")
+	if len(splitToken) == 2 {
+		return splitToken[1]
+	}
+	return ""
+}
+
+func getToken(ctx *gin.Context) (*jwt.Token, error) {
+	tokenString := getTokenFromRequest(ctx)
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("gagal signin method : %v", t.Header["alg"])
+		}
+		return privateKey, nil
+	})
+	return token, err
 }
--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"diary_api/controller"
 	"diary_api/database"
 	"diary_api/model"
+	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	loadEnv()
 	loadDB()
+	serveApplication()
 }
 
 func loadDB() {
@@ -24,4 +28,15 @@ func loadEnv() {
 	if err != nil {
 		log.Fatal("gagal membaca file .env")
 	}
+}
+
+func serveApplication() {
+	router := gin.Default()
+
+	publicRoutes := router.Group("/auth")
+	publicRoutes.POST("/register", controller.Register)
+	publicRoutes.POST("/login", controller.Login)
+
+	router.Run(":7878")
+	fmt.Println("Server berjalan di port 7878")
 }
